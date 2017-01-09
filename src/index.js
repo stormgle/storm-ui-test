9de47e9614d345a480db3eceafdc4fc73eceddba
duongtdn <duongtdn@gmail.com>
2017-01-09 14:03:32 +0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+
+import { Page } from 'react-storm-ui'
+
+class App extends Component {
+	
+	render() {
+		const data = [];
+		for (let i = 1; i < 100; i++) {
+			data.push(`Line ${i}`);
+		}		
+		return (
+			<Page 
+				renderHeader = {() => (<h3> Header </h3>)}
+				renderFooter = {() => (<h3> Footer </h3>)} > 
+				
+				<div> Content </div>
+				
+				{ 
+					data.map( (entry,id) => <div key = {id} > {entry} </div> ) 
+				}
+				
+			</Page>
+		);
+	}
+	
+}
+
+
+render( <App />, document.getElementById('root'));
